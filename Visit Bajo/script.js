83fbc1bf1dd7a577497500faf3c0a9a724b22159
sheny-ayu <shenyayuanugrah@gmail.com>
2024-12-12 13:28:28 +0700

    menu sidebar click

--- Visit Bajo/script.js
+++ Visit Bajo/script.js
@@ -4,3 +4,12 @@ const navbarNav = document.querySelector(".navbar-nav");
 document.querySelector("#menu").onclick = () => {
   navbarNav.classList.toggle("active");
 };
+
+//Out sidebar
+const menu = document.querySelector("#menu");
+
+document.addEventListener("click", function (e) {
+  if (!menu.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
