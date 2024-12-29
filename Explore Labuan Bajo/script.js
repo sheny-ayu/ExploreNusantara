899bae45e0f51eaeab3a6915524f2c95db924d93
sheny-ayu <shenyayuanugrah@gmail.com>
2024-12-29 22:23:32 +0700

    night

---
+++ Explore Labuan Bajo/script.js
@@ -0,0 +1,21 @@
+//SLIDER SECTION
+//Toggle class active
+const navbarNav = document.querySelector(".navbar-nav");
+//When menu is clicked
+document.querySelector("#menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+//Out sidebar
+const menu = document.querySelector("#menu");
+
+document.addEventListener("click", function (e) {
+  if (!menu.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
+
+// NOW INFO SECTION
+
+//CLOCK SECTION
+
